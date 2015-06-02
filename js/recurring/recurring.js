9187000decb1e8e35c41f2a21f83962108bcad96
DonnellC <DonnellC@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 00:07:52 +0000

    Rename folder
    git-svn-id: https://plugins.svn.wordpress.org/globe-gateway-e4/tags/0.0.7@1172330 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/recurring/recurring.js
@@ -0,0 +1,6 @@
+function recurringTRUE() {
+    if (document.getElementById('recurring).TRUE) {
+        document.getElementById('recurring_billing_TRUE').style.visibility = 'visible';	
+    }
+    else document.getElementById('recurring_billing_TRUE').style.visibility = 'hidden';
+	}
